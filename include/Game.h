63e7bdcf31dee8ec96c53ada8b8ebfc1ddaad56c
Tim <sterzeltim@gmail.com>
2016-05-21 17:12:55 +0200

    Add Game class

--- include/Game.h
+++ include/Game.h
@@ -1,14 +1,22 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <SFML/Graphics.hpp>
 
 class Game
 {
     private:
+        sf::RenderWindow m_window;
+        sf::CircleShape m_shape;
+
+
+    private:
+        void processEvents();
+        void render();
 
     public:
         Game();
-
+        void run();
 };
 
 #endif // GAME_H
--- src/Game.cpp
+++ src/Game.cpp
@@ -1,7 +1,41 @@
 #include "Game.h"
+#include <iostream>
 
-Game::Game()
+Game::Game() :
+m_window(sf::VideoMode(800, 480), "ArenaSFML")
+,m_shape(100.f)
 {
-    //ctor
+    m_shape.setFillColor(sf::Color::Green);
+}
+
+void Game::processEvents()
+{
+    sf::Event event;
+    while (m_window.pollEvent(event))
+    {
+        if (event.type == sf::Event::Closed)
+        {
+            m_window.close();
+        }
+    }
+}
+
+void Game::render()
+{
+    std::cout << "Render" << std::endl;
+    m_window.clear();
+    m_window.draw(m_shape);
+    m_window.display();
+
+}
+
+void Game::run()
+{
+    while (m_window.isOpen())
+    {
+        processEvents();
+        render();
+    }
+
 }
 
