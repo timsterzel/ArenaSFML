6b51dce377ac4f3908848c0b16bc704e2ccdc356
Tim <sterzeltim@gmail.com>
2016-05-29 18:01:39 +0200

    Add drawCurrent method for Warrior class

--- include/Warrior.hpp
+++ include/Warrior.hpp
@@ -1,13 +1,16 @@
 #ifndef WARRIOR_HPP
 #define WARRIOR_HPP
+#include <SFML/Graphics.hpp>
+#include "Entity.hpp"
 
-
-class Warrior
+class Warrior : public Entity
 {
+    private:
+        sf::Sprite m_sprite;
+
     public:
         Warrior();
-    protected:
-    private:
+        virtual void drawCurrent(sf::RenderTarget &target, sf::RenderStates states) const;
 };
 
 #endif // WARRIOR_HPP
--- src/Warrior.cpp
+++ src/Warrior.cpp
@@ -4,3 +4,8 @@ Warrior::Warrior()
 {
 
 }
+
+void Warrior::drawCurrent(sf::RenderTarget &target, sf::RenderStates states) const
+{
+    target.draw(m_sprite, states);
+}
