36e3aca761745c1d4b97b2a0d53a6e9f3ffd40dc
Tim <sterzeltim@gmail.com>
2016-06-28 16:58:16 +0200

    Store pointer to collided SceneNodes in CollisionInfo class

--- include/Collision/CollisionInfo.hpp
+++ include/Collision/CollisionInfo.hpp
@@ -1,6 +1,7 @@
 #ifndef COLLISIONINFO_HPP
 #define COLLISIONINFO_HPP
 #include <SFML/Graphics.hpp>
+#include "Components/SceneNode.hpp"
 
 class CollisionInfo
 {
@@ -8,13 +9,18 @@ class CollisionInfo
         bool m_isCollision;
         float m_length;
         sf::Vector2f m_direction;
+        SceneNode *m_collidedFirst;
+        SceneNode *m_collidedSecond;
+
     public:
         explicit CollisionInfo(bool isCollision);
-        CollisionInfo(bool isCollision, float length, sf::Vector2f direction);
+        CollisionInfo(bool isCollision, float length, sf::Vector2f direction, SceneNode *collidedFirst, SceneNode *collidedSecond);
 
         bool isCollision() const;
         float getLength() const;
         sf::Vector2f getDirection() const;
+        SceneNode* getCollidedFirst() const;
+        SceneNode* getCollidedSecond() const;
 };
 
 #endif // COLLISIONINFO_HPP
--- src/Collision/CollisionInfo.cpp
+++ src/Collision/CollisionInfo.cpp
@@ -4,14 +4,18 @@ CollisionInfo::CollisionInfo(bool isCollision)
 : m_isCollision{ isCollision }
 , m_length{ 0.f }
 , m_direction{ 0.f, 0.f }
+, m_collidedFirst{ nullptr }
+, m_collidedSecod { nullptr }
 {
 
 }
 
-CollisionInfo::CollisionInfo(bool isCollision, float length, sf::Vector2f direction)
+CollisionInfo::CollisionInfo(bool isCollision, float length, sf::Vector2f direction, SceneNode *collidedFirst, SceneNode *collidedSecond))
 : m_isCollision{ isCollision }
 , m_length{ length }
 , m_direction{ direction }
+, m_collidedFirst{ collidedFirst }
+, m_collidedSecod { collidedSecond }
 {
 
 }
@@ -29,4 +33,14 @@ float CollisionInfo::getLength() const
 sf::Vector2f CollisionInfo::getDirection() const
 {
     return m_direction;
+}
+
+SceneNode* CollisionInfo::getCollidedFirst() const
+{
+    return m_collidedFirst;
+}
+
+SceneNode* CollisionInfo::getCollidedSecond() const
+{
+    return m_collidedSecond;
 }
