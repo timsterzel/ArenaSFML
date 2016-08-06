5c96e368d9df0a39e3dabe63c5c93013d912c06e
Tim <sterzeltim@gmail.com>
2016-08-06 16:01:16 +0200

    Load intRect fata from map txt file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include "Game.hpp"
-
+#include "Resources/SpriteSheetMapLoader.hpp"
 int main()
 {
+    SpriteSheetMapLoader::loadRectData("assets/sprites/Warriors/knight.txt");
     Game game(true, true);
     game.run();
 
