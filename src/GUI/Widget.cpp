73afad4f0da18247ab76fdd110419ca8edd5e804
Tim <sterzeltim@gmail.com>
2016-09-28 19:30:48 +0200

    Create Base class Widget for GUI

---
+++ src/GUI/Widget.cpp
@@ -0,0 +1,88 @@
+#include "GUI/Widget.hpp"
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+
+Widget::Widget()
+: m_parent{ nullptr }
+//, m_isCollisionCheckOn{ true }
+{
+
+}
+
+Widget::~Widget()
+{
+
+}
+
+void Widget::attachChild(Ptr child)
+{
+    child->m_parent = this;
+    m_children.push_back(std::move(child));
+}
+
+Widget::Ptr Widget::detachChild(const Widget& node)
+{
+    auto found = std::find_if(m_children.begin(), m_children.end(), [&] (Ptr &p) -> bool { return p.get() == &node; });
+    // There is an error when we try to detach a child which does not exists,so stop execution in debug mode
+    assert(found != m_children.end());
+
+    Ptr result = std::move(*found);
+    result->m_parent = nullptr;
+    m_children.erase(found);
+    return result;
+}
+
+void Widget::draw(sf::RenderTarget &target, sf::RenderStates states) const
+{
+        states.transform *= getTransform();
+        drawCurrent(target, states);
+        drawChildren(target, states);
+}
+
+void Widget::drawCurrent(sf::RenderTarget &target, sf::RenderStates states) const
+{
+    //Do nothing by default
+}
+
+void Widget::drawChildren(sf::RenderTarget &target, sf::RenderStates states) const
+{
+    for (const Ptr &child : m_children)
+    {
+        child->draw(target, states);
+    }
+}
+
+void Widget::update(float dt)
+{
+    updateCurrent(dt);
+    updateChildren(dt);
+}
+
+void Widget::updateCurrent(float dt)
+{
+    // Do nothing by default
+}
+
+void Widget::updateChildren(float dt)
+{
+    for (const Ptr &child : m_children)
+    {
+        child->update(dt);
+    }
+}
+
+sf::Transform Widget::getWorldTransform() const
+{
+    sf::Transform trform = { sf::Transform::Identity };
+    for (const Widget *node = this; node != nullptr; node = node->m_parent)
+    {
+        trform = node->getTransform() * trform;
+    }
+    return trform;
+}
+
+sf::Vector2f Widget::getWorldPosition() const
+{
+    return getWorldTransform() * sf::Vector2f();
+}
